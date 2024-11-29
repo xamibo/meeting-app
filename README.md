6619da6189b409ec0eb1df4bc6876e75e6d208af
xamibo <xamibose2001@gmail.com>
2024-11-29 22:49:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Full Stack Calendly Clone with Next JS, React, Tailwind CSS, Prisma, Neon, Clerk, Shadcn UI Tutorial 🔥🔥
+## https://youtu.be/glAC7d-TYSM
+
+![image](https://github.com/user-attachments/assets/0d4b2ce7-ebfb-4e31-a48e-a394c2cb3ddc)
+
+### Make sure to create a `.env` file with following variables -
+
+```
+DATABASE_URL=
+NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=
+CLERK_SECRET_KEY=
+
+NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+```
---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/header";
+import CreateEventDrawer from "@/components/create-event";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Schedulrr",
+  description: " ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by Soumik</p>
+            </div>
+          </footer>
+          <CreateEventDrawer />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
